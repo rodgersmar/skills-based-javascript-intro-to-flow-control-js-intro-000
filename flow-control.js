61c59203cc8207019bbb09714fb8f9cde7e0b9e0
toto <ro.marcus@free.fr>
2019-11-08 16:06:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,7 +12,13 @@ function teenager(age) {
 }}
 
 function ageChecker(age) {
-
+  if( age >= 14 &&  age <= 19) {
+    return ("You are a teenager!");
+  } else if ( age >= 12) {
+    return ("You are a kid");
+  } else {
+    return ("You are a grownup");
+  }
 
 }
 
